172751ebaae4f287684341557972c06401f97fd0
Maxim-Snoeys <r0792870@student.thomasmore.be>
2022-03-02 17:33:39 +0100

    halve push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkblack': '#17171E',
+        'lightblack': '#24212A',
+        'darkpurple': '#4D3A4D',
+        'lightpurple': "#AE83A5",
+        'lightgrey': "#E7E7E7",
+      },
+      textTransform: {
+
+      },
+      fontFamily: {
+        'titel': ['Anton', 'sans-serif'],
+        'text': ['Raleway', 'sans-serif']
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
 }
